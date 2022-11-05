1cb6fdd48fdfb91f3f70b716a52e0bbd3f01769f
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-11-05 21:37:42 +0530

    Add files via upload

--- roller.js
+++ roller.js
@@ -1,6 +1,6 @@
-document.write('<script src="https://cdn.jsdelivr.net/gh/abtp2/gw@latest/components.js" defer></script>');
-document.write('<script src="https://cdn.jsdelivr.net/gh/abtp2/gw@latest/swiper.js" defer></script>');
-document.write('<script src="https://cdn.jsdelivr.net/gh/abtp2/gw@latest/animation.js" defer></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/abtp2/gw@latest/components.js"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/abtp2/gw@latest/swiper.js"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/abtp2/gw@latest/animation.js"></script>');
 
 console.log("Initializing rollercss...");
 setTimeout(function(){
